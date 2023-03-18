f7c439d6eb5e6d00ca3982569deee5db334df564
Ahmed Bahgat <ahbahgat01@gmail.com>
2023-03-18 22:58:49 +0200

    creation of the project

--- Examination_System/StudentDashboard.cs
+++ Examination_System/StudentDashboard.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Examination_System
 {
-    public partial class Form1 : Form
+    public partial class StudentDashboard : Form
     {
-        public Form1()
+        public StudentDashboard()
         {
             InitializeComponent();
         }
